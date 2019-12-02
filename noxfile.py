2c177cbc2abf307fc589d3d4cfeb27ca82132ab6
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2019-12-02 20:41:18 +0000

    Disable nox reuse venv on lint

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ nox.options.stop_on_first_error = True
 source_files = ("socksio", "tests", "setup.py", "noxfile.py")
 
 
-@nox.session(reuse_venv=True)
+@nox.session()
 def lint(session):
     session.install("autoflake", "black", "flake8", "isort", "seed-isort-config")
 

4ee777a074f7fc47589cd1135f5aacf0685e906e
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2020-01-21 09:46:44 +0000

    Type annotate singledispatchmethod

--- socksio/singledispatchmethod.py
+++ socksio/singledispatchmethod.py
@@ -5,6 +5,7 @@ removing 2.7 specific code.
 """
 
 import functools
+import typing
 
 
 if hasattr(functools, "singledispatchmethod"):
@@ -19,26 +20,36 @@ else:
     class singledispatchmethod(object):  # type: ignore
         """Single-dispatch generic method descriptor."""
 
-        def __init__(self, func):
+        def __init__(self, func: typing.Callable):
             if not callable(func) and not hasattr(func, "__get__"):
                 raise TypeError("{!r} is not callable or a descriptor".format(func))
 
             self.dispatcher = singledispatch(func)
             self.func = func
 
-        def register(self, cls, method=None):
+        def register(
+            self,
+            cls: typing.Callable,
+            method: typing.Callable[[typing.Any], typing.Any],
+        ) -> typing.Callable:
             return self.dispatcher.register(cls, func=method)
 
-        def __get__(self, obj, cls):
-            def _method(*args, **kwargs):
-                method = self.dispatcher.dispatch(args[0].__class__)
+        def __get__(
+            self, obj: typing.Any, cls: typing.Callable[[typing.Any], typing.Any]
+        ) -> typing.Callable:
+            def _method(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+                method = self.dispatcher.dispatch(args[0].__class__)  # type: typing.Any
                 return method.__get__(obj, cls)(*args, **kwargs)
 
-            _method.__isabstractmethod__ = self.__isabstractmethod__
-            _method.register = self.register
+            # The type: ignore below is due to `_method` being given a strict
+            # "Callable[[VarArg(Any), KwArg(Any)], Any]" which causes a
+            # 'has no attribute "__isabstractmethod__" error'
+            # felt safe enough to ignore
+            _method.__isabstractmethod__ = self.__isabstractmethod__  # type: ignore
+            _method.register = self.register  # type: ignore
             update_wrapper(_method, self.func)
             return _method
 
         @property
-        def __isabstractmethod__(self):
+        def __isabstractmethod__(self) -> typing.Any:
             return getattr(self.func, "__isabstractmethod__", False)
